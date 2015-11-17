1e8626d4f93fe71b2af752cf8652c4650f6d8dea
Ramsey <rdarling42@gmail.com>
2015-11-17 12:17:57 -0500

    adding starter theme

---
+++ wp-content/themes/knight_wallace/template-parts/content-search.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying results in search pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package knight_wallace
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php knight_wallace_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+
+	<footer class="entry-footer">
+		<?php knight_wallace_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
