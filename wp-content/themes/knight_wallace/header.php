be46e70f6e3a47d269d718a8791f2764e352fd8f
Ramsey <rdarling42@gmail.com>
2016-01-08 15:49:59 -0500

    logo fix

--- wp-content/themes/knight_wallace/header.php
+++ wp-content/themes/knight_wallace/header.php
@@ -22,7 +22,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site fellows-page">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'knight_wallace' ); ?></a>
   <header id="header">
     <div class="row" id="eyebrow">
       <div class="large-12 columns">
@@ -36,7 +35,7 @@
     <div class="row">
       <div class="large-4 columns">
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <img src="<?php echo get_template_directory(); ?>/assets/images/uofmlogo.png" alt="Knight-Wallace" />
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/uofmlogo.png" alt="Knight-Wallace" />
         </a>
       </div>
       <div class="large-8 columns">
@@ -44,10 +43,8 @@
       </div>
     </div>
   </header>
-
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'knight_wallace' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+<nav id="site-navigation" class="main-navigation" role="navigation">
+    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'knight_wallace' ); ?></button>
+    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+</nav><!-- #site-navigation -->
 
