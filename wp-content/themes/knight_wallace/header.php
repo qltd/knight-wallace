444534e53953b154499790d5047231c4f704e7df
Ramsey <rdarling42@gmail.com>
2016-01-11 16:14:20 -0500

    dynamically displaying main nav items

--- wp-content/themes/knight_wallace/header.php
+++ wp-content/themes/knight_wallace/header.php
@@ -43,8 +43,62 @@
       </div>
     </div>
   </header>
-<nav id="site-navigation" class="main-navigation" role="navigation">
-    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'knight_wallace' ); ?></button>
-    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+<nav id="main_nav">
+<?php 
+//Grab Main nav item items
+$main_menu = wp_get_nav_menu_items('primary');
+$ni1_link = !empty($main_menu[0]->url) ? $main_menu[0]->url : "Menu Item";
+$ni1_title = !empty($main_menu[0]->title) ? $main_menu[0]->title : "Menu Item";
+$ni2_link = !empty($main_menu[1]->url) ? $main_menu[1]->url : "Menu Item";
+$ni2_title = !empty($main_menu[1]->title) ? $main_menu[1]->title : "Menu Item";
+$ni3_link = !empty($main_menu[2]->url) ? $main_menu[2]->url : "Menu Item";
+$ni3_title = !empty($main_menu[2]->title) ? $main_menu[2]->title : "Menu Item";
+?>
+    <div class="row">
+      <div class="large-4 columns">
+        <p class="primary active wallace-house" data-sub-nav-menu="wallace-house">
+        <a href="<?php echo $ni1_link; ?>"><?php echo $ni1_title; ?></a>
+        </p>
+      </div>
+      <div class="large-4 columns">
+        <p class="primary knight-wallace-fellows" data-sub-nav-menu="knight-wallace-fellows">
+            <a href="<?php echo $ni2_link; ?>"><?php echo $ni2_title; ?></a>
+        </p>
+      </div>
+      <div class="large-4 columns">
+        <p class="primary livingston-awards" data-sub-nav-menu="livingston-awards">
+            <a href="<?php echo $ni3_link; ?>"><?php echo $ni3_title; ?></a>
+        </p>
+      </div>
+    </div>
+    <div class="sub-nav-wrap">
+      <div class="row">
+        <div class="large-12 columns">
+<?php $sub_menu_one = wp_get_nav_menu_items('sub-menu-one'); ?>
+          <ul class="wallace-house">
+<?php if(!empty($sub_menu_one)): ?>
+    <?php foreach($sub_menu_one as $smo_obj): ?>
+        <li><a href="<?php echo $smo_obj->url; ?>"><?php echo $smo_obj->title; ?></a></li>
+    <?php endforeach;?>
+<?php endif; ?>
+          </ul>
+          <ul class="knight-wallace-fellows disappear">
+            <li><a href="">About</a></li>
+            <li><a href="">How To Apply</a></li>
+            <li><a href="">Our Fellows</a></li>
+            <li><a href="">Board of Directors</a></li>
+            <li><a href="">Sign In</a></li>
+          </ul>
+          <ul class="livingston-awards disappear">
+            <li><a href="">About</a></li>
+            <li><a href="">Entry</a></li>
+            <li><a href="">Judges</a></li>
+            <li><a href="">Winners</a></li>
+            <li><a href="">Finalists</a></li>
+            <li><a href="">Clurman Award</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
 </nav><!-- #site-navigation -->
 
