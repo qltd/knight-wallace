57fa4510f48c4d6e509286a135b2fcb88c7642b4
RamseyDarling <rdarling42@gmail.com>
2016-01-18 10:18:17 -0500

    nav menu fix

--- wp-content/themes/knight_wallace/header-livingston.php
+++ wp-content/themes/knight_wallace/header-livingston.php
@@ -17,7 +17,7 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 <?php wp_head(); ?>
-<script src="<?php echo get_template_directory(); ?>/assets/bower_components/modernizr/modernizr.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/bower_components/modernizr/modernizr.js"></script>
 </head>
 
 <body <?php body_class(); ?> id="livingstion_page">
