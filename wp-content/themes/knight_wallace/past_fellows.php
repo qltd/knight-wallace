a39afa65307bb75e9f1691b73cd9075782d08ba0
RamseyDarling <rdarling42@gmail.com>
2016-02-10 11:57:22 -0500

    working past fellows template

---
+++ wp-content/themes/knight_wallace/past_fellows.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Template Name: Past Fellows 
+ * 
+ *
+ * @package knight_wallace
+ */
+
+get_header('fellows'); ?>
+
+<?php 
+include_once('helpers.php');
+//grab our junk
+$this_page_meta = get_post_meta($post->ID);
+$this_year = !empty($this_page_meta['year']) ? $this_page_meta['year'][0] : null;
+$alerts = get_posts(array('category_name'=>'alert'));
+$fellows = get_posts(array('post_type'=>'person_kw_fellow','posts_per_page'=> -1));
+$sorted_fellows = sort_fellows_by_year($fellows, $this_year);
+?>
+
+<section class="breadcrumb">
+<div class="row">
+    <div class="small-6 columns">
+        <a href="/knight-wallace/our-fellows/" class="library-link">Current Fellows</a>
+    </div>
+</div>
+</section>
+
+<div class="row">
+    <div class="large-12 columns">
+        <h1 class="text-center">Past Fellows</h1>
+    </div>
+</div>
+
+<?php if(!is_null($this_year)): ?>
+<section class="year-control">
+    <?php $count = 1;?>
+    <div class="row">
+        <?php for($i=$this_year;$i>1990;$i--): ?>
+            <div class="medium-3 columns">
+                <a href=""><?php echo $i - 1 .'-'. $i; ?></a>
+            </div>
+            <?php if($count == 4): ?>
+                </div>
+                <div class="row">
+                <?php $count = 0; ?>
+            <?php endif; ?>
+            <?php $count += 1; ?>
+        <?php endfor; ?>
+    </div>
+</section>
+<div class="row">
+    <div class="large-12 columns">
+    <h2 class="text-center"><?php echo $this_year - 1 .'-'. $this_year; ?> Fellows</h2>
+    </div>
+</div>
+<?php endif; ?>
+
+<main id="main" class="site-main post-main" role="main">
+    <div class="row">
+        <div class="large-12 columns">
+            <div class="fellow-class-image"><?php echo get_the_post_thumbnail($post->ID); ?></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="large-10 columns large-offset-2">
+            <?php if(!empty($sorted_fellows)): ?>
+                <?php foreach($sorted_fellows as $fellow): ?>
+                    <div class="row">
+                        <div class="large-12 columns">
+                            <p class="fellow-name">
+                                <a href="<?php echo $fellow['link']; ?>" class="fellow-link">
+                                <?php echo $fellow['first_name']; ?>&nbsp;
+                                <?php echo $fellow['last_name']; ?></a>
+                            </p>
+                            <p class="fellow-title">
+                            <?php echo $fellow['title']; ?>
+                            </p>
+                            <p class="fellow-bio">
+                            <?php echo $fellow['bio']; ?>
+                            </p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</main>
+
+<?php 
+//display alerts
+if(!empty($alerts)):
+?>
+<section id="alerts">
+<?php foreach($alerts as $alert): ?>
+<div class="row alert">
+  <div class="large-4 columns">
+    <p>
+    <strong><?php echo $alert->post_title; ?></strong>
+      <br /><?php echo $alert->post_excerpt; ?></p>
+  </div>
+  <div class="large-8 columns">
+  <p><?php echo $alert->post_content; ?></p>
+  </div>
+</div>
+<?php endforeach; ?>
+</section>
+<?php endif; ?>
+
+
+<?php get_footer('fellows'); ?>
