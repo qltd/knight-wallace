09f809dba0d7ddf8da0a6ca3a80dee970d93de3a
Ramsey <rdarling42@gmail.com>
2016-04-14 08:35:13 -0400

    upping memory limit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('memory_limit', '512M');
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
