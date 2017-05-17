9dc26657f872cade7f048a0636a35df9e3e830ef
Matt Restorff <inperpetuum@gmail.com>
2017-05-17 11:38:21 -0400

    Fixed library header and social sharing links

--- wp-content/themes/knight_wallace/library.php
+++ wp-content/themes/knight_wallace/library.php
@@ -82,7 +82,7 @@ $sorted_libs = sort_library_items($libs);
 <?php else: ?>
     <div class="row">
         <div class="large-12">
-            <h1 class="text-center">Library</h1>
+            <h1 class="entry-title text-center">Library</h1>
         </div>
     </div>
 <?php endif; ?>
