fd93281098e43eb0956b7ad5d3f843f376e97aac
Matt Restorff <inperpetuum@gmail.com>
2017-07-07 15:12:24 -0400

    Tweaked async function

--- wp-content/themes/knight_wallace/functions.php
+++ wp-content/themes/knight_wallace/functions.php
@@ -1255,6 +1255,6 @@ function q_async_scripts($url)
     else if ( is_admin() )
         return str_replace( '#asyncload', '', $url );
     else
-    return str_replace( '#asyncload', '', $url )."' defer='defer' async='async";
+    return str_replace( '#asyncload', '', $url )."' async='async";
     }
 add_filter( 'clean_url', 'ikreativ_async_scripts', 11, 1 );
