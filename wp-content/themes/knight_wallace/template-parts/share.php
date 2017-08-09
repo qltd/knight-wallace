b50cf0238fe5d6d42f3b359934094ca5a34ad341
Matt Restorff <inperpetuum@gmail.com>
2017-08-09 12:03:31 -0400

    Modified twitter share button to use meta tags

--- wp-content/themes/knight_wallace/template-parts/share.php
+++ wp-content/themes/knight_wallace/template-parts/share.php
@@ -1,6 +1,8 @@
 <div class="small-6 columns text-right">
     <p class="share-wrap">Share:
-        <a href="https://twitter.com/intent/tweet?text=<?php urlencode(the_title()); ?> https://<?php echo urlencode($_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
+        <?php /* <a href="https://twitter.com/intent/tweet?text=<?php urlencode(the_title()); ?> https://<?php echo urlencode($_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); ?>" target="_blank"><i class="fa fa-twitter"></i></a> */ ?>
+        <a href="https://twitter.com/share?
+url=<?php echo urlencode($_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); ?>"><i class="fa fa-twitter"></i></a>
         <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); ?>" target="_blank"><i class="fa fa-facebook"></i></a>
         <a href="mailto:?subject=[Shared from wallacehouse.umich.edu] <?php the_title();?>&body=<?php the_permalink();?>"><i class="fa fa-envelope"></i></a>
     </p>
