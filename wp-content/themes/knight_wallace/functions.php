46610ad2f266b8a8c93c1de7f798e9d365dd0b7e
paperrobots <inperpetuum@gmail.com>
2020-11-30 15:46:41 -0500

    Added cookie conscent plugin

--- wp-content/themes/knight_wallace/functions.php
+++ wp-content/themes/knight_wallace/functions.php
@@ -1011,10 +1011,6 @@ function knight_wallace_scripts() {
   wp_enqueue_style('fancybox-css', 'https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css');
   wp_enqueue_script('fancybox-js', 'https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js', array('jquery'), false, true);
    
-
-
-
-
     wp_enqueue_script( 'knight_wallace-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
     wp_enqueue_script( 'knight_wallace-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
